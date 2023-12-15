a82d71aa64ac8488a575597a2eb224cbfad97afd
Igorlozko <g00390368@atu.ie>
2023-12-15 15:34:02 +0000

    updated Map

--- app/componenets/modals/RentModal.tsx
+++ app/componenets/modals/RentModal.tsx
@@ -4,6 +4,11 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 
 enum STEPS {
@@ -21,6 +26,40 @@ const RentModal = ()=>{
 
     const [step, setStep] = useState(STEPS.CATEGORY);
 
+    const{
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState:{
+            errors,
+        },
+        reset
+    } = useForm<FieldValues>({
+        defaultValues:{
+            category:'',
+            location: null,
+            guestCount: 1,
+            roomCount: 1,
+            bathroomCount:1,
+            imageSrc:'',
+            price: 1,
+            title: '',
+            description: ''
+        }
+    });
+
+    const category = watch('category');
+    const location = watch('location');
+
+    const setCustomValue = (id: string, value: any) =>{
+        setValue(id, value,{
+            shouldValidate:true,
+            shouldDirty: true,
+            shouldTouch: true,
+        })
+    }
+
     const onBack = () => {
         setStep((value) => value - 1);
     };
@@ -35,12 +74,12 @@ const RentModal = ()=>{
         return 'Next';
     },[step]);
 
-   //const secondaryActionLable = useMemo(()=>{
-   // if(step == STEPS.CATEGORY){
-   //     return undefined;
-   // }
-   // return 'Back';
-   //},[step])
+   const secondaryActionLable = useMemo(() => {
+        if(step == STEPS.CATEGORY){
+            return undefined;
+        }
+        return 'Back';
+   },[step])
 
     let bodyContent = (
         <div className="flex dlex-col gap-8">
@@ -48,15 +87,53 @@ const RentModal = ()=>{
                 title = "Which of these best describes your Gear"
                 subtitle="Pick a category"
             />
+            <div
+                className ="
+                grid
+                grid-cols-1
+                md:grid-cols-2
+                gap-3
+                max-h-[50vh]
+                overflow-y-auto
+                "
+            >
+                {categories.map((item) =>(
+                    <div key = {item.label} className ="col-span-1" >
+                        <CategoryInput
+                            onClick={(category)=> setCustomValue('category',category)}
+                            selected = {category == item.label}
+                            label={item.label}
+                            icon={item.icon}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     )
+
+    if (step == STEPS.LOCATION){
+        bodyContent =(
+            <div className = "flex flex-col gap-8">
+                <Heading 
+                title=" Where is your gear located"
+                subtitle="Help renters find you"                
+                />
+                <CountrySelect 
+                    value={location}
+                    onChange={(value)=> setCustomValue('location', value)}
+                />
+                <Map />
+            </div>
+        )
+    }
+
     return (
         <Modal
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLable={actionLable}
-     //   secondaryActionLable={secondaryActionLable}
+        secondaryActionLable={secondaryActionLable}
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
         title = "My Gear Repo"
         body = {bodyContent}
