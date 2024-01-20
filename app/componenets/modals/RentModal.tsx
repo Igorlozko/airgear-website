25d84acf8c87e4b4f31c520bb8a28e3ff5761446
Igorlozko <g00390368@atu.ie>
2024-01-20 12:53:30 +0000

    finished the entery to new gear listings

--- app/componenets/modals/RentModal.tsx
+++ app/componenets/modals/RentModal.tsx
@@ -4,13 +4,17 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
-import { categories } from "../navbar/Categories";
+import Categories, { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValue, FieldValues, useForm } from "react-hook-form";
+import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 
 enum STEPS {
@@ -24,9 +28,11 @@ enum STEPS {
 
 
 const RentModal = ()=>{
+    const router = useRouter();
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
 
     const{
         register,
@@ -41,7 +47,7 @@ const RentModal = ()=>{
         defaultValues:{
             category:'',
             location: null,
-            renterCount: 1,
+            guestCount: 1,
             imageSrc:'',
             price: 1,
             title: '',
@@ -51,7 +57,7 @@ const RentModal = ()=>{
 
     const category = watch('category');
     const location = watch('location');
-    const renterCount = watch('renterCount');
+    const guestCount = watch('guestCount');
     const imageSrc = watch('imageSrc');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
@@ -72,6 +78,26 @@ const RentModal = ()=>{
     const onNext = () =>{
         setStep((value) => value + 1);
     };
+    const onSubmit: SubmitHandler<FieldValues> = (data)=>{
+        if (step != STEPS.PRICE){
+            return onNext();
+        }
+        setIsLoading(true);
+
+        axios.post('/api/listings', data)
+        .then(()=>{
+            toast.success('Listing Created!');
+            router.refresh();
+            reset();
+            setStep(STEPS.CATEGORY);
+            rentModal.onClose();
+        })
+        .catch(()=>{
+            toast.error('Something went wrong');
+        }).finally(()=>{
+            setIsLoading(false);
+        })
+    }
 
     const actionLable = useMemo(() => {
         if(step == STEPS.PRICE){
@@ -122,7 +148,7 @@ const RentModal = ()=>{
             <div className = "flex flex-col gap-8">
                 <Heading 
                 title=" Where is your gear located"
-                subtitle="Help renters find you"                
+                subtitle="Help guests find you"                
                 />
                 <CountrySelect 
                     value={location}
@@ -143,10 +169,10 @@ const RentModal = ()=>{
                     subtitle ="What is the specifications"
                 />
                 <Counter
-                    title ="Renters"
-                    subtitle="How many renters do you allow per item ?"
-                    value={renterCount}
-                    onChange={(value) => setCustomValue('renterCount',value)}
+                    title ="guests"
+                    subtitle="How many guests do you allow per item ?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount',value)}
                 />
             </div>
         )
@@ -157,11 +183,58 @@ const RentModal = ()=>{
             <div className="flex flex-col gap-8">
                 <Heading
                     title = "Add photos of your gear"
-                    subtitle="Show renters what your gear looks like"
+                    subtitle="Show guests what your gear looks like"
                 />
                 <ImageUpload
                     value={imageSrc}
                     onChange={(value)=> setCustomValue('imageSrc', value)}
+                />
+            </div>
+        )
+    }
+    if(step == STEPS.DESCRIPTION){
+        bodyContent =(
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title = "Describe your gear"
+                    subtitle="Keep it short and simple"
+                />
+                <Input
+                    id="title"
+                    label="title"
+                    disabled={isLoading}
+                    register={register}
+                    errors = {errors}
+                    required
+                />
+                <hr />
+                <Input
+                    id="description"
+                    label="description"
+                    disabled={isLoading}
+                    register={register}
+                    errors = {errors}
+                    required
+                />
+            </div>
+        )
+    }
+    if(step == STEPS.PRICE){
+        bodyContent =(
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title = "Set your price"
+                    subtitle="How much do you charge ?"
+                />
+                <Input
+                    id="price"
+                    label="price"
+                    formatPrice={true}
+                    type="number"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
                 />
             </div>
         )
@@ -171,7 +244,7 @@ const RentModal = ()=>{
         <Modal
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={onNext}
+        onSubmit={handleSubmit(onSubmit)}
         actionLable={actionLable}
         secondaryActionLable={secondaryActionLable}
         secondaryAction={step == STEPS.CATEGORY ? undefined : onBack}
